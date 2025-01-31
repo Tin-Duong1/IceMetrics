c62ae5921e9a0419de68b25a15d0c5472c64c3bb
Tin Duong <tin.duong405@gmail.com>
2025-01-31 11:44:09 -0600

    added database sqlmodels code

--- server/datatabase/models.py
+++ server/datatabase/models.py
@@ -1,5 +1,16 @@
 from typing import Annotated
 
 from fastapi import Depends, FastAPI, HTTPException, Query
-from sqlmodel import Field, Session, SQLModel, create_engine, select
+from sqlmodel import Field, Session, SQLModel, create_engine, select, Column, LargeBinary
 
+class User(SQLModel, table=True):
+    user_id: int = Field(primary_key=True)
+    name : str = Field(max_length=255)
+    email: str = Field(max_length=255)
+    hashed_password: str = Field(max_length=255)
+    
+    
+class UserVideos(SQLModel, table=True):
+    img_id: int = Field(primary_key=True)
+    video: bytes = Field(sa_column=Column(LargeBinary))
+    user_id: int = Field(foreign_key="user.id")
