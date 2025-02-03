2a97f5dfeda74856dee98b1ff1303c71c4117a3d
Jackson Morgan <jake81morgan@gmail.com>
2025-02-02 21:54:35 -0600

    added auth router route to control sign in and sign out routes to the web app

--- server/server.py
+++ server/server.py
@@ -1,15 +1,21 @@
 from fastapi import FastAPI
+from auth import router as auth_router
 from fastapi.middleware.cors import CORSMiddleware
 
+# creates the FastAPI instance
 app = FastAPI()
 
+# origins that are allowed to make requests to the server
 origins = [
     "http://localhost.tiangolo.com",
     "https://localhost.tiangolo.com",
     "http://localhost",
     "http://localhost:8080",
+    "http://localhost:3000",
+    "http://localhost:5173"
 ]
 
+# adds the CORS middleware to the app
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -18,7 +24,12 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# connects to the authentication routes
+app.include_router(auth_router)
+
+# root route
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
 
