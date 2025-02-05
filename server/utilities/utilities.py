784a858431f99bcd14785eb07127ea7add21b612
Tin Duong <tin.duong405@gmail.com>
2025-02-05 14:37:18 -0600

    added util functions for data retrieval

--- server/utilities/utilities.py
+++ server/utilities/utilities.py
@@ -1,3 +1,13 @@
 from sqlmodel import select
+from sqlalchemy.orm import Session
+from database.models import User, UserVideos, VideoData
 
-def get_user(db: Session, 
+def get_user_by_id(db: Session, user_id: int) -> User:
+    return db.exec(select(User).where(User.id == user_id)).first()
+
+def get_video_by_id(db: Session, video_id: int) -> VideoData:
+    return db.exec(select(UserVideos).where(UserVideos.id == video_id)).first()
+
+def get_all_videos_by_user_id(db: Session, user_id: int) -> list[UserVideos]:
+    return db.exec(select(UserVideos).where(UserVideos.id == user_id)).first()
+
