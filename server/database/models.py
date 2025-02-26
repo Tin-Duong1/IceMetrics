42477edbd0fc537c5307a11a16006e1fbf099781
Tin Duong <tin.duong405@gmail.com>
2025-02-26 12:32:10 -0600

    added the info for videos into the database

--- server/database/models.py
+++ server/database/models.py
@@ -2,10 +2,20 @@ from typing import Annotated, Optional
 from datetime import datetime
 
 from fastapi import Depends, FastAPI, HTTPException, Query
-from sqlmodel import Field, Session, SQLModel, create_engine, select, Column, LargeBinary
+from sqlmodel import Field, Session, SQLModel, create_engine, select, Column, Relationship
 
 class UserInfo(SQLModel, table=True):
     user_id: int = Field(primary_key=True)
     name : str = Field(max_length=255)
     email: str = Field(max_length=255)
     password: str = Field(max_length=255)
+    videos: list["VideoStatistics"] = Relationship(back_populates="user")
+
+class VideoStatistics(SQLModel, table=True):
+    video_id: int = Field(primary_key=True)
+    title: str = Field(max_length=255)
+    description: str = Field(max_length=255)
+    user_id : int = Field(foreign_key="userinfo.user_id")
+    user : UserInfo = Relationship(back_populates="videos")
+    
+    
