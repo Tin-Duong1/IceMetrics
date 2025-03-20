c53d8f9b676727a14c4f0e52c8dcb89067e4e39b
Jackson Morgan <jake81morgan@gmail.com>
2025-03-20 15:39:01 -0500

    Update Home.tsx

--- client/src/components/dashboard/Home/Home.tsx
+++ client/src/components/dashboard/Home/Home.tsx
@@ -1,54 +1,20 @@
-import { Box, Card, Stack, Typography } from "@mui/material";
-import { Gauge } from "@mui/x-charts";
+import { Stack } from "@mui/material";
 import StatCard from "../Cards/StatCard";
+import TopCards from "../Cards/TopCards";
 
 function Home() {
   return (
-    <Stack direction={"row"} sx={{ gap: 2, padding: 2, width: "100%" }}>
-      <StatCard>
-        <Typography variant="h6">Videos Uploaded</Typography>
-        <Gauge
-          width={200}
-          height={200}
-          value={1}
-          valueMax={10}
-          valueMin={0}
-          innerRadius={50}
-        />
-      </StatCard>
-      <StatCard>
-        <Typography variant="h6">Videos Uploaded</Typography>
-        <Gauge
-          width={200}
-          height={200}
-          value={1}
-          valueMax={10}
-          valueMin={0}
-          innerRadius={50}
-        />
-      </StatCard>
-      <StatCard>
-        <Typography variant="h6">Videos Uploaded</Typography>
-        <Gauge
-          width={200}
-          height={200}
-          value={1}
-          valueMax={10}
-          valueMin={0}
-          innerRadius={50}
-        />
-      </StatCard>
-      <StatCard>
-        <Typography variant="h6">Videos Uploaded</Typography>
-        <Gauge
-          width={200}
-          height={200}
-          value={1}
-          valueMax={10}
-          valueMin={0}
-          innerRadius={50}
-        />
-      </StatCard>
+    <Stack
+      direction={"column"}
+      spacing={2}
+      sx={{ gap: 0, padding: 2, width: "100%" }}
+    >
+      <TopCards />
+      <Stack direction={"row"} gap={2}>
+        <StatCard />
+        <StatCard />
+        <StatCard />
+      </Stack>
     </Stack>
   );
 }
